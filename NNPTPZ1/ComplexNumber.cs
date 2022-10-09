de2e05d6bbc4a0819152cd292b35cf746f7d1cb3
Rssnobody <silvia.cmilanska@gmail.com>
2022-10-09 15:36:55 +0200

    Code refactoring

---
+++ NNPTPZ1/ComplexNumber.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace NNPTPZ1.Mathematics {
+    public class ComplexNumber {
+        public double RealPart { get; set; }
+        public float ImaginaryPart { get; set; }
+
+        public static readonly ComplexNumber Zero = new ComplexNumber() {
+            RealPart = 0,
+            ImaginaryPart = 0
+        };
+
+        public override bool Equals(object obj) {
+            if (obj is ComplexNumber) {
+                ComplexNumber complexNumber = obj as ComplexNumber;
+                return complexNumber.RealPart == RealPart && complexNumber.ImaginaryPart == ImaginaryPart;
+            }
+            return base.Equals(obj);
+        }
+
+        public ComplexNumber Multiply(ComplexNumber multiplicand) {
+            return new ComplexNumber() {
+                RealPart = this.RealPart * multiplicand.RealPart - this.ImaginaryPart * multiplicand.ImaginaryPart,
+                ImaginaryPart = (float)(this.RealPart * multiplicand.ImaginaryPart + this.ImaginaryPart * multiplicand.RealPart)
+            };
+        }
+
+        public double GetAbsoluteValue() {
+            return Math.Sqrt(RealPart * RealPart + ImaginaryPart * ImaginaryPart);
+        }
+
+        public ComplexNumber Add(ComplexNumber addend) {
+            return new ComplexNumber() {
+                RealPart = this.RealPart + addend.RealPart,
+                ImaginaryPart = this.ImaginaryPart + addend.ImaginaryPart
+            };
+        }
+
+        public double GetAngleInRadians() {
+            return Math.Atan(ImaginaryPart / RealPart);
+        }
+
+        public ComplexNumber Subtract(ComplexNumber subtrahend) {
+            return new ComplexNumber() {
+                RealPart = this.RealPart - subtrahend.RealPart,
+                ImaginaryPart = this.ImaginaryPart - subtrahend.ImaginaryPart
+            };
+        }
+
+        public override string ToString() {
+            return $"({RealPart} + {ImaginaryPart}i)";
+        }
+
+        internal ComplexNumber Divide(ComplexNumber number) {
+            var dividend = this.Multiply(new ComplexNumber() { RealPart = number.RealPart, ImaginaryPart = -number.ImaginaryPart });
+            var divisor = Math.Pow(number.RealPart, 2) + Math.Pow(number.ImaginaryPart, 2);
+
+            return new ComplexNumber() {
+                RealPart = dividend.RealPart / divisor,
+                ImaginaryPart = (float)(dividend.ImaginaryPart / divisor)
+            };
+        }
+    }
+}
---
+++ NNPTPZ1/Polynomial.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+
+namespace NNPTPZ1.Mathematics {
+    public class Polynomial {
+        /// <summary>
+        /// Coefficients
+        /// </summary>
+        public List<ComplexNumber> Coefficients { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Polynomial() => Coefficients = new List<ComplexNumber>();
+
+        public void Add(ComplexNumber coefficient) =>
+            Coefficients.Add(coefficient);
+
+        /// <summary>
+        /// Derives this polynomial and creates new one
+        /// </summary>
+        /// <returns>Derived polynomial</returns>
+        public Polynomial Derive() {
+            Polynomial newPolynomial = new Polynomial();
+            for (int i = 1; i < Coefficients.Count; i++) {
+                newPolynomial.Coefficients.Add(Coefficients[i].Multiply(new ComplexNumber() { RealPart = i }));
+            }
+            return newPolynomial;
+        }
+
+        /// <summary>
+        /// Evaluates polynomial at given point
+        /// </summary>
+        /// <param name="point">point of evaluation</param>
+        /// <returns>y</returns>
+        public ComplexNumber Eval(double point) {
+            return Eval(new ComplexNumber() { RealPart = point, ImaginaryPart = 0 });
+        }
+
+        /// <summary>
+        /// Evaluates polynomial at given point
+        /// </summary>
+        /// <param name="point">point of evaluation</param>
+        /// <returns>y</returns>
+        public ComplexNumber Eval(ComplexNumber point) {
+            ComplexNumber result = ComplexNumber.Zero;
+            for (int i = 0; i < Coefficients.Count; i++) {
+                ComplexNumber coefficient = Coefficients[i];
+                ComplexNumber newPoint = point;
+
+                if (i > 0) {
+                    for (int j = 0; j < i - 1; j++)
+                        newPoint = newPoint.Multiply(point);
+
+                    coefficient = coefficient.Multiply(newPoint);
+                }
+                result = result.Add(coefficient);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// ToString
+        /// </summary>
+        /// <returns>String representation of polynomial</returns>
+        public override string ToString() {
+            string result = "";
+            for (int i = 0; i < Coefficients.Count; i++) {
+                result += Coefficients[i];
+                if (i > 0) {
+                    for (int j = 0; j < i; j++) {
+                        result += "x";
+                    }
+                }
+                if (i + 1 < Coefficients.Count)
+                    result += " + ";
+            }
+            return result;
+        }
+    }
+}
