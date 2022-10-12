524f34f72bbe89552ebbbfa907191be2a3986ec5
Petr Bednář <p.petr.bednar@gmail.com>
2022-10-13 00:02:25 +0200

    Added structures
    Added structure classes to contain used values

---
+++ NNPTPZ1/Structure/MinMax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NNPTPZ1.Structure
+{
+    public class MinMax<T>
+    {
+        public T Min { get; set; }
+        public T Max { get; set; }
+
+        public MinMax(T min, T max)
+        {
+            Min = min;
+            Max = max;
+        }
+    }
+}
---
+++ NNPTPZ1/Structure/Size.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NNPTPZ1.Structure
+{
+    public class Size<T>
+    {
+        public T Width { get; set; }
+        public T Height { get; set; }
+
+        public Size(T width, T height)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+}
