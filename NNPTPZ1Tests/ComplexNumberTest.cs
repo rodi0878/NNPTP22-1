c848cfff43a1b66d12960f5efefaf05bf38697cc
Lukáš Janáček <st58238@upce.cz>
2022-10-23 16:44:11 +0200

    Renamed a few tests.

--- NNPTPZ1Tests/ComplexNumberTest.cs
+++ NNPTPZ1Tests/ComplexNumberTest.cs
@@ -14,7 +14,7 @@ namespace NNPTPZ1.Mathematics.Tests
     {
 
         [TestMethod()]
-        public void AddTest()
+        public void ComplexNumberAddTest1()
         {
             ComplexNumber a = new ComplexNumber()
             {
@@ -28,60 +28,125 @@ namespace NNPTPZ1.Mathematics.Tests
             };
 
             ComplexNumber actual = a.Add(b);
-            ComplexNumber shouldBe = new ComplexNumber()
+            ComplexNumber expected = new ComplexNumber()
             {
                 RealPart = 11,
                 ImaginaryPart = 22
             };
 
-            Assert.AreEqual(shouldBe, actual);
+            Assert.AreEqual(expected, actual);
+        }
 
-            var e2 = "(10 + 20i)";
-            var r2 = a.ToString();
-            Assert.AreEqual(e2, r2);
-            e2 = "(1 + 2i)";
-            r2 = b.ToString();
-            Assert.AreEqual(e2, r2);
-
-            a = new ComplexNumber()
+        [TestMethod()]
+        public void ComplexNumberAddTest2()
+        {
+            ComplexNumber a = new ComplexNumber()
             {
                 RealPart = 1,
                 ImaginaryPart = -1
             };
-            b = new ComplexNumber() { RealPart = 0, ImaginaryPart = 0 };
-            shouldBe = new ComplexNumber() { RealPart = 1, ImaginaryPart = -1 };
-            actual = a.Add(b);
-            Assert.AreEqual(shouldBe, actual);
-
-            e2 = "(1 + -1i)";
-            r2 = a.ToString();
-            Assert.AreEqual(e2, r2);
-
-            e2 = "(0 + 0i)";
-            r2 = b.ToString();
-            Assert.AreEqual(e2, r2);
+            ComplexNumber b = new ComplexNumber()
+            {
+                RealPart = 1,
+                ImaginaryPart = 2
+            };
+            ComplexNumber expected = new ComplexNumber() { RealPart = 2, ImaginaryPart = 1 };
+            ComplexNumber actual = a.Add(b);
+            Assert.AreEqual(expected, actual);
         }
 
         [TestMethod()]
-        public void AddTestPolynome()
+        public void ComplexNumberToStringTest1() {
+            ComplexNumber a = new ComplexNumber()
+            {
+                RealPart = 10,
+                ImaginaryPart = 20
+            };
+
+            string expected = "(10 + 20i)";
+            string actual = a.ToString();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ComplexNumberToStringTest2()
+        {
+            ComplexNumber b = new ComplexNumber()
+            {
+                RealPart = 1,
+                ImaginaryPart = 2
+            };
+            string expected = "(1 + 2i)";
+            string actual = b.ToString();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ComplexNumberToStringTest3()
+        {
+            ComplexNumber a = new ComplexNumber()
+            {
+                RealPart = 1,
+                ImaginaryPart = -1
+            };
+            string expected = "(1 + -1i)";
+            string actual = a.ToString();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ComplexNumberToStringTest4()
+        {
+            ComplexNumber b = new ComplexNumber() { RealPart = 0, ImaginaryPart = 0 };
+
+            string expected = "(0 + 0i)";
+            string actual = b.ToString();
+            Assert.AreEqual(expected, actual);
+        }
+
+        private Polygon GetDefaultPolygon()
         {
             Polygon poly = new Polygon();
             poly.Coefficient.Add(new ComplexNumber() { RealPart = 1, ImaginaryPart = 0 });
             poly.Coefficient.Add(new ComplexNumber() { RealPart = 0, ImaginaryPart = 0 });
             poly.Coefficient.Add(new ComplexNumber() { RealPart = 1, ImaginaryPart = 0 });
+            return poly;
+        }
+
+        [TestMethod()]
+        public void ComplexNumberPolygonAddPolynomeTest()
+        {
+            Polygon poly = GetDefaultPolygon();
             ComplexNumber result = poly.Evaluate(new ComplexNumber() { RealPart = 0, ImaginaryPart = 0 });
             var expected = new ComplexNumber() { RealPart = 1, ImaginaryPart = 0 };
             Assert.AreEqual(expected, result);
-            result = poly.Evaluate(new ComplexNumber() { RealPart = 1, ImaginaryPart = 0 });
-            expected = new ComplexNumber() { RealPart = 2, ImaginaryPart = 0 };
-            Assert.AreEqual(expected, result);
-            result = poly.Evaluate(new ComplexNumber() { RealPart = 2, ImaginaryPart = 0 });
-            expected = new ComplexNumber() { RealPart = 5.0000000000, ImaginaryPart = 0 };
-            Assert.AreEqual(expected, result);
+        }
 
-            var r2 = poly.ToString();
-            var e2 = "(1 + 0i) + (0 + 0i)x + (1 + 0i)xx";
-            Assert.AreEqual(e2, r2);
+        [TestMethod()]
+        public void ComplexNumberPolygonEvaluateTest1()
+        {
+            Polygon poly = GetDefaultPolygon();
+            ComplexNumber result = poly.Evaluate(new ComplexNumber() { RealPart = 1, ImaginaryPart = 0 });
+            ComplexNumber expected = new ComplexNumber() { RealPart = 2, ImaginaryPart = 0 };
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void ComplexNumberPolygonEvaluateTest2()
+        {
+            Polygon poly = GetDefaultPolygon();
+            ComplexNumber result = poly.Evaluate(new ComplexNumber() { RealPart = 2, ImaginaryPart = 0 });
+            ComplexNumber expected = new ComplexNumber() { RealPart = 5.0000000000, ImaginaryPart = 0 };
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void ComplexNumberPolygonToStringTest()
+        {
+            Polygon poly = GetDefaultPolygon();
+            var actual = poly.ToString();
+            var expected = "(1 + 0i) + (0 + 0i)x + (1 + 0i)xx";
+            Assert.AreEqual(expected, actual);
         }
     }
 }
