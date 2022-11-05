3325745f53e164de22ca1151e2a2edc3198ec390
tomik27 <42244137+tomik27@users.noreply.github.com>
2022-11-05 16:10:20 +0100

    Add testsg

---
+++ NNPTPZ1Tests/ComplexNumberTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NNPTPZ1;
+
+namespace NNPTPZ1.Mathematics.Tests {
+        [TestClass()]
+        public class ComplexNumberTests {
+
+            [TestMethod()]
+            public void ToStringTestComplexNumberRe0Im0() {
+                ComplexNumber complexNumber = new ComplexNumber(0, 0);
+                var expectedString = "(0 + 0i)";
+                var resultString = complexNumber.ToString();
+
+                Assert.AreEqual(expectedString, resultString);
+            }
+            [TestMethod()]
+            public void ToStringTestComplexNumberRe10Im20() {
+                ComplexNumber complexNumber = new ComplexNumber(10, 20);
+                var expectedString = "(10 + 20i)";
+                var resultString = complexNumber.ToString();
+
+                Assert.AreEqual(expectedString, resultString);
+            }
+            [TestMethod()]
+            public void ToStringTestComplexNumberRe1Im2() {
+                ComplexNumber complexNumber = new ComplexNumber(1, 2);
+                var expectedString = "(1 + 2i)";
+                var resultString = complexNumber.ToString();
+
+                Assert.AreEqual(expectedString, resultString);
+            }
+            [TestMethod()]
+            public void ToStringTestComplexNumberRe1NegativeIm1() {
+                ComplexNumber complexNumber = new ComplexNumber(1, -1);
+                var expectedString = "(1 + -1i)";
+                var resultString = complexNumber.ToString();
+
+                Assert.AreEqual(expectedString, resultString);
+            }
+            [TestMethod()]
+            public void AddTestComplexNumberRe10Im20AndRe1Im2() {
+                ComplexNumber complexNumber = new ComplexNumber(10, 20);
+                ComplexNumber actualNumber = complexNumber.Add(new ComplexNumber(1, 2));
+                ComplexNumber expectedNumber = new ComplexNumber(11, 22);
+
+                Assert.AreEqual(expectedNumber, actualNumber);
+
+            }
+            [TestMethod()]
+            public void AddTestComplexNumberRe1NegativeIm1AndRe0Im0() {
+                ComplexNumber complexNumber = new ComplexNumber(1, -1);
+                ComplexNumber actualNumber = complexNumber.Add(new ComplexNumber(0, 0));
+                ComplexNumber expectedNumber = new ComplexNumber(1, -1);
+                Assert.AreEqual(expectedNumber, actualNumber);
+
+            }
+        }
+    }
+
+
---
+++ NNPTPZ1Tests/PolynomialTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NNPTPZ1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace NNPTPZ1.Mathematics.Tests {
+        [TestClass]
+        public class PolynomialTests {
+
+            [TestMethod()]
+            public void AddTestPolynome() {
+                Polynomial polynom = new Mathematics.Polynomial();
+                polynom.Coeficients.Add(new ComplexNumber(1, 0));
+                polynom.Coeficients.Add(new ComplexNumber(0, 0));
+                polynom.Coeficients.Add(new ComplexNumber(1, 0));
+
+                var resultString = polynom.ToString();
+                var expectedString = "(1 + 0i) + (0 + 0i)x + (1 + 0i)xx";
+
+                Assert.AreEqual(expectedString, resultString);
+            }
+            [TestMethod()]
+            public void EvaluateTestPolynomeOnValueZero() {
+                Polynomial polynom = new Mathematics.Polynomial();
+                polynom.Coeficients.Add(new ComplexNumber(1, 0));
+                polynom.Coeficients.Add(new ComplexNumber(0, 0));
+                polynom.Coeficients.Add(new ComplexNumber(1, 0));
+
+                ComplexNumber resultValue = polynom.Evaluate(new ComplexNumber(0, 0));
+                var expectedValue = new ComplexNumber(1, 0);
+
+                Assert.AreEqual(expectedValue, resultValue);
+            }
+            [TestMethod()]
+            public void EvaluateTestPolynomeOnValueOne() {
+                Polynomial polynom = new Mathematics.Polynomial();
+                polynom.Coeficients.Add(new ComplexNumber(1, 0));
+                polynom.Coeficients.Add(new ComplexNumber(0, 0));
+                polynom.Coeficients.Add(new ComplexNumber(1, 0));
+
+                ComplexNumber resultValue = polynom.Evaluate(new ComplexNumber(1, 0));
+                var expectedValue = new ComplexNumber(2, 0);
+
+                Assert.AreEqual(expectedValue, resultValue);
+            }
+            [TestMethod()]
+            public void EvaluateTestPolynomeOnValueTwo() {
+                Polynomial polynom = new Mathematics.Polynomial();
+                polynom.Coeficients.Add(new ComplexNumber(1, 0));
+                polynom.Coeficients.Add(new ComplexNumber(0, 0));
+                polynom.Coeficients.Add(new ComplexNumber(1, 0));
+
+                ComplexNumber resultValue = polynom.Evaluate(new ComplexNumber(2, 0));
+                var expectedValue = new ComplexNumber(5.0000000000, 0);
+
+                Assert.AreEqual(expectedValue, resultValue);
+            }
+        }
+    }
