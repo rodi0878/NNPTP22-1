0d5582f7df2099714f5d3feb861228662c1f5cd5
Lukáš Janáček <st58238@upce.cz>
2022-11-08 10:29:13 +0100

    Canged data type.

--- NNPTPZ1/ComplexNumber.cs
+++ NNPTPZ1/ComplexNumber.cs
@@ -5,7 +5,7 @@ namespace NNPTPZ1.Mathematics
     public class ComplexNumber
     {
         public double RealPart { get; set; }
-        public float ImaginaryPart { get; set; }
+        public double ImaginaryPart { get; set; }
 
         public override bool Equals(object obj)
         {
